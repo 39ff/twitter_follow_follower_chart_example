0501ae9af5e7c2663644e99b56b75fd9643b4792
kouhei <itkitjp@gmail.com>
2015-11-28 22:54:08 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+/**
+ * Created by PhpStorm.
+ * User: kouhei
+ * Date: 15/11/28
+ * Time: 18:35
+ */
+define('DBHOST','127.0.0.1');
+define('DBUSER','');
+define('DBPASS','');
+define('DBNAME','');
+define('DBCHARSET','utf8');
+
+//api settings
+define('CK','');
+define('CS','');
+define('AT','');
+define('ATS','');
